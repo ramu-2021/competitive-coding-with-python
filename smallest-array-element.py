472898d7d7d2254125db66225bdadce3ef75b9c2
ramu-2021 <72304507+ramu-2021@users.noreply.github.com>
2020-10-03 18:40:20 +0530

    Update smallest-array-element.py

--- smallest-array-element.py
+++ smallest-array-element.py
@@ -2,10 +2,8 @@
 Write a function return smallest element in the array
 '''
 def my_fun(arr,n):
-    my_min=10**9
-    for i in arr:
-        my_min=min(i,my_min)
-    return my_min
+    arr.sort()
+    return arr[0]
 n=int(input())
 arr=list(map(int,input().split()))
 print(my_fun(arr,n))
